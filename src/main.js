0ab446fb7442135f204c1a9a2746136890a8bdab
Emma-xiaochen <736202625@qq.com>
2021-03-07 16:53:46 +0800

    引入ivew-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI);
 
 Vue.config.productionTip = false
 
